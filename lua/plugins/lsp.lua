b632338600e3f49a548bbda8d09473b14b52c46a
reffined <alireza.karampoor@gmail.com>
2024-11-05 14:27:54 +0000

    added rename symbol keymap

---
+++ lua/plugins/lsp.lua
@@ -0,0 +1,6 @@
+return {
+  "neovim/nvim-lspconfig",
+  keys = {
+    { "<leader>r", vim.lsp.buf.rename, desc = "Rename Symbol" },
+  },
+}
--- lua/plugins/test-plugin.lua
+++
@@ -1,3 +0,0 @@
-return {
-  "Reffined/nvim-test-plugin",
-}
