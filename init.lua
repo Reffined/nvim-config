48b8f49343ac45019b9ad4943ee0c4c2b064b541
reffined <alireza.karampoor@gmail.com>
2024-11-21 17:13:14 +0330

    from work

--- init.lua
+++ init.lua
@@ -1,3 +1,14 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
 require("luasnip.loaders.from_lua").load({ paths = { "~/snippets" } })
+local lspconfig = require("lspconfig")
+
+lspconfig.helm_ls.setup({
+  settings = {
+    ["helm-ls"] = {
+      yamlls = {
+        path = "yaml-language-server",
+      },
+    },
+  },
+})
