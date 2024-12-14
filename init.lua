7292bf57d2b91d96064d01e459571fe736f2f265
havok <alireza.karampoor@gmail.com>
2024-12-14 18:43:28 +0330

    home

--- init.lua
+++ init.lua
@@ -44,5 +44,7 @@ lspconfig.omnisharp.setup({
     },
   },
 })
+
 vim.api.nvim_set_keymap("n", "<C-m>", ":delm!<CR>", {})
 vim.api.nvim_set_keymap("n", "<leader>m", ":MarkdownPreview<CR>", {})
+vim.api.nvim_set_keymap("n", "<leader>tg", ":!templ generate<CR>", { desc = "templ generate" })
